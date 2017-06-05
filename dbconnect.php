d3caa14acdd0f4ea9dfe3b129312b754c77e9ded
jinwenyang <amyyhr1211@gmail.com>
2017-06-05 21:42:02 +0800

    更正]

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php
 $hostname = "localhost";
 $user = "root";
-$password = "o937o24537";
-$dbname = "phphw";
+$password = "calltheshot";
+$dbname = "cccamp";
 $link = @mysqli_connect($hostname, $user, $password, $dbname);
 mysqli_query($link, 'SET NAMES UTF-8');
 mysqli_query($link,"set character set 'utf8'");
